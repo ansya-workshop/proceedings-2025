c12a543c33736cb15fba18bf90945648d51d87ef
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-11-20 13:24:03 +0100

    feat: update index.html

--- print-pages.sh
+++ print-pages.sh
@@ -3,7 +3,8 @@ CURRENT=1
 function count_pages {
     PAGES=`pdfinfo $1 | grep Pages: | awk '{print $2}'`
     END=`expr $CURRENT + $PAGES - 1`
-    echo "<span class=\"CEURPAGES\">$CURRENT–$END</span>"
+    # echo "<span class=\"CEURPAGES\">$CURRENT–$END</span>"
+    echo "$1: $CURRENT–$END"
     CURRENT=`expr $END + 1`
 }
 
@@ -13,6 +14,9 @@ function count_pages {
 # for N in ${CUSTOM_ORDER[@]}; do
 # for N in `seq 1 18`; do
 # for P in `cat index.html | grep href=\"WOA_2025_P | awk -F '"' '{print $2}' | awk -F 'r' '{print $2}'`; do
-for P in `cat list.txt`; do
-    count_pages $P
+for P in `seq 1 30`; do
+    if [ -f paper_$P.pdf ]; then
+        FILE=paper_$P.pdf
+        count_pages $FILE
+    fi
 done
